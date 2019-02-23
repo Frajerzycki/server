cc46beaef154ffd7f383ae0db9f9e19fae780ec5
frajerzycki <frajerzycki@protonmail.com>
2019-02-23 20:33:56 +0100

    Works.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
+from tinydb import TinyDB
+from tinydb.middlewares import CachingMiddleware
 app = Flask(__name__)
+db = TinyDB("data.json")
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
-from app import app
-from flask import request
+from app import app, db
+from flask import request, render_template, url_for
+from tinydb.storages import JSONStorage
+from datetime import datetime
+import time
 @app.route('/')
 def test():
     return 'Test!\n'
 
 @app.route('/info', methods=['POST'])
 def info():
+    data = request.json
+    data['id'] = int(round(time.time() * 1000))
+    db.insert(data)
     return request.json["temperature"]
 
+@app.route("/weather")
+def weather():
+    return render_template('weather.html', w=db.all(),datetime=datetime, int=int)
